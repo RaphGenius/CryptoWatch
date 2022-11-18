ee137369748cf5617e41f52eb71417a027da49a8
Raphael <guet.raphael@hotmail.com>
2022-11-18 19:04:40 +0100

    Ajout de la  boite au survoles contentant le graphique du coin + choix dans la periode

--- src/components/TableLine.js
+++ src/components/TableLine.js
@@ -1,9 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
+import CoinChart from "./CoinChart";
 import PercentChange from "./PercentChange";
 import StarIcon from "./StarIcon";
 
 const TableLine = ({ coin, index }) => {
-  // Permet
+  const [showChart, setShowChart] = useState(false);
+
   const priceFormater = (price) => {
     //Transformation du chiffre en string, on ne peut connaitre la longueur d'un numb
     if (Math.round(price).toString().length < 4) {
@@ -28,8 +30,15 @@ const TableLine = ({ coin, index }) => {
           <img src={coin.image} height="20" alt="logo" />
         </div>
         <div className="infos">
-          <div className="chart-img">
+          <div
+            className="chart-img"
+            onMouseEnter={() => setShowChart(true)}
+            onMouseLeave={() => setShowChart(false)}
+          >
             <img src="./assets/chart-icon.svg" alt="chart-icon" />
+            <div className="chart-container" id={coin.name}>
+              {showChart && <CoinChart coinId={coin.id} coinName={coin.name} />}
+            </div>
           </div>
           <h4>{coin.name} </h4>
           <span>- {coin.symbol.toUpperCase()} </span>
