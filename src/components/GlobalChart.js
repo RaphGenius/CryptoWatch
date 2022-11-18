fdfddf53ff346f0c9e70e918341a48cb9859a7cd
Raphael <guet.raphael@hotmail.com>
2022-11-18 12:13:51 +0100

    Fin de la treemap, couleur s'affichage en fonction du %, exclusion des stable coins

--- src/components/GlobalChart.js
+++ src/components/GlobalChart.js
@@ -1,27 +1,59 @@
 import React, { useEffect, useState } from "react";
 import { Tooltip, Treemap } from "recharts";
-
+import colors from "../styles/_settings.scss";
 const GlobalChart = ({ coinsData }) => {
   const [dataArray, setDataArray] = useState([]);
-
+  const colorPicker = (number) => {
+    if (number >= 20) {
+      return colors.color1;
+    } else if (number >= 5) {
+      return colors.green2;
+    } else if (number >= 0) {
+      return colors.green1;
+    } else if (number >= -5) {
+      return colors.red1;
+    } else if (number >= -20) {
+      return colors.red2;
+    } else {
+      return colors.black2;
+    }
+  };
+  const excludeCoin = (coin) => {
+    if (
+      coin === "usdt" ||
+      coin === "usdc" ||
+      coin === "busd" ||
+      coin === "dai" ||
+      coin === "ust" ||
+      coin === "mim"
+    ) {
+      return false;
+    } else {
+      return true;
+    }
+  };
   useEffect(() => {
     let charData = [];
 
     if (coinsData.length > 0) {
       for (let i = 0; i < 45; i++) {
-        charData.push({
-          name:
-            coinsData[i].symbol.toUpperCase() +
-            " " +
-            coinsData[i].market_cap_change_percentage_24h.toFixed(1) +
-            "%",
-          size: coinsData[i].market_cap,
-          fill: null,
-        });
+        if (excludeCoin(coinsData[i].symbol))
+          charData.push({
+            name:
+              coinsData[i].symbol.toUpperCase() +
+              " " +
+              coinsData[i].market_cap_change_percentage_24h.toFixed(1) +
+              "%",
+            size: coinsData[i].market_cap,
+            // Couleur définit par le chiffre reçu, puis mis dans la func
+            fill: colorPicker(coinsData[i].price_change_percentage_24h),
+          });
       }
+      console.log(charData);
     }
     setDataArray(charData);
   }, [coinsData]);
+  // Parametre de l'affichage du panneau au survol de la treemap
   const TreemapToolTip = ({ active, payload }) => {
     if (active && payload && payload.length) {
       return (
@@ -35,6 +67,7 @@ const GlobalChart = ({ coinsData }) => {
 
   return (
     <div className="global-chart">
+      {/* Paramètres de la TreeMap  */}
       <Treemap
         width={730}
         height={181}
