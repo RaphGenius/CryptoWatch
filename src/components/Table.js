8e46884c13fc36f60008714374f019d548fe399d
Raphael <guet.raphael@hotmail.com>
2022-11-18 15:07:55 +0100

    Ajout du header de table + logique de trie // Pas encore d'affichage des coins

---
+++ src/components/Table.js
@@ -0,0 +1,62 @@
+import React, { useState } from "react";
+
+const Table = ({ coinsData }) => {
+  const [rangeNumber, setRangeNumber] = useState(100);
+  const [orderBy, setOrderBy] = useState("");
+  const tableHeader = [
+    "Prix",
+    "MarketCap",
+    "Volume",
+    "1h",
+    "1j",
+    "1m",
+    "6m",
+    "1a",
+    "ATH",
+  ];
+
+  return (
+    <div className="table-container">
+      <div className="table-header">
+        <div className="range-container">
+          <span>
+            Top{" "}
+            <input
+              type="text"
+              value={rangeNumber}
+              onChange={(e) => setRangeNumber(e.target.value)}
+            />{" "}
+          </span>
+          <input
+            type="range"
+            min={1}
+            max={250}
+            value={rangeNumber}
+            onChange={(e) => setRangeNumber(e.target.value)}
+          />
+        </div>
+        {tableHeader.map((el) => (
+          <li key={el}>
+            <input
+              type="radio"
+              name="header-el"
+              id={el}
+              defaultChecked={
+                el === orderBy || el === orderBy + "reverse" ? true : false
+              }
+              onClick={() => {
+                setOrderBy(el);
+                if (orderBy === el) {
+                  setOrderBy(el + "reverse");
+                }
+              }}
+            />
+            <label htmlFor={el}>{el} </label>
+          </li>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Table;
