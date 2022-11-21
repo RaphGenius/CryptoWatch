21ec958a4499c760cd18ab7bd2267d37f64fef4d
Raphael <guet.raphael@hotmail.com>
2022-11-21 15:15:04 +0100

    Ajout de la treemap au survol des crypto

--- src/components/CoinChart.js
+++ src/components/CoinChart.js
@@ -1,9 +1,18 @@
-import axios from "axios";
 import React, { useEffect, useState } from "react";
+import axios from "axios";
+import {
+  AreaChart,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  Area,
+} from "recharts";
+import colors from "../styles/_settings.scss";
 
 const CoinChart = ({ coinId, coinName }) => {
+  const [coinData, setCoinData] = useState();
   const [duration, setDuration] = useState(30);
-
   const headerData = [
     [1, "1 jour"],
     [3, "3 jours"],
@@ -16,31 +25,68 @@ const CoinChart = ({ coinId, coinName }) => {
   ];
 
   useEffect(() => {
+    let dataArray = [];
+
     axios
       .get(
         `https://api.coingecko.com/api/v3/coins/${coinId}/market_chart?vs_currency=usd&days=${duration}${
           duration > 32 ? "&interval=daily" : ""
         }`
       )
-      .then((res) => console.log(res));
-  }, []);
+      .then((res) => {
+        for (let i = 0; i < res.data.prices.length; i++) {
+          let price = res.data.prices[i][1];
+
+          dataArray.push({
+            date: new Date(res.data.prices[i][0]).toLocaleDateString(),
+            price: price < "50" ? price : parseInt(price),
+          });
+        }
+        setCoinData(dataArray);
+      });
+  }, [coinId, duration]);
+
   return (
     <div className="coin-chart">
-      <p>{coinName} </p>
+      <p>{coinName}</p>
       <div className="btn-container">
-        {headerData.map((el) => {
+        {headerData.map((radio) => {
           return (
             <div
-              key={el[0]}
-              htmlFor={"btn" + el[0]}
-              onClick={() => setDuration(el[0])}
-              className={el[0] === duration ? "active-btn" : ""}
+              htmlFor={"btn" + radio[0]}
+              onClick={() => setDuration(radio[0])}
+              key={radio[0]}
+              className={radio[0] === duration ? "active-btn" : ""}
             >
-              {el[1]}
+              {radio[1]}
             </div>
           );
         })}
       </div>
+      <AreaChart
+        width={680}
+        height={250}
+        data={coinData}
+        margin={{ top: 10, right: 0, left: 100, bottom: 0 }}
+      >
+        <defs>
+          <linearGradient id="colorUv" x1="0" y1="0" x2="0" y2="1">
+            <stop offset="7%" stopColor={colors.color1} stopOpacity={0.8} />
+            <stop offset="93%" stopColor={colors.white1} stopOpacity={0} />
+          </linearGradient>
+        </defs>
+        <XAxis dataKey="date" />
+        <YAxis domain={["auto", "auto"]} />
+        <CartesianGrid strokeDasharray="3 3" />
+        <Tooltip />
+        <Area
+          type="monotone"
+          dataKey="price"
+          stroke={colors.color1}
+          fillOpacity={1}
+          fill="url(#colorUv)"
+        />
+      </AreaChart>
     </div>
   );
 };
